2857861d257a487012114e71a7c169175c9df97b
Arina Babinskya <rianawilson94@gmail.com>
2026-04-18 21:45:59 +0300

    some changes

--- include/int128.h
+++ include/int128.h
@@ -11,9 +11,9 @@ private:
     uint64_t low_;
 
 public:
-
     Int128();
     Int128(int64_t v);
+    explicit Int128(std::string_view str);
 
     explicit operator int64_t() const ;
     explicit operator double() const ;
@@ -25,16 +25,27 @@ public:
     Int128& operator*=(const Int128& other);
     Int128& operator/=(const Int128& other);
 
+    Int128 operator+(const Int128& other) const;
+    Int128 operator-(const Int128& other) const;
+    Int128 operator*(const Int128& other) const;
+    Int128 operator/(const Int128& other) const;
+
     Int128 operator-() const;
 
-    friend bool operator==(const Int128&, const Int128&);
-    friend bool operator!=(const Int128&, const Int128&);
-    friend bool operator<(const Int128& a, const Int128& b);
+    bool operator==(const Int128& other);
+    bool operator!=(const Int128& other);
 
     friend std::ostream& operator<<(std::ostream&, const Int128&);
-};
 
-Int128 operator+(Int128, const Int128&);
-Int128 operator-(Int128, const Int128&);
-Int128 operator*(Int128, const Int128&);
-Int128 operator/(Int128, const Int128&);
+    bool operator<(const Int128& other);
+    bool operator<=(const Int128& other);
+
+    bool operator>(const Int128& other);
+    bool operator>=(const Int128& other);
+
+    Int128 operator<<(int shift) const;
+    Int128& operator<<=(int shift);
+
+    Int128 operator>>(int shift) const;
+    Int128& operator>>=(int shift);
+};
--- src/int128.cpp
+++ src/int128.cpp
@@ -4,9 +4,28 @@
 #include <string_view>
 
 Int128::Int128() : high_(0), low_(0) {}
+
 Int128::Int128(int64_t v) : low_(v) {
     if (v < 0) { high_ = -1; } 
     else { high_ = 0; }
+}
+
+Int128::Int128(std::string_view str) : high_(0), low_(0) {
+    bool neg = false;
+    size_t start = 0;
+    if (str[0] == '-') {
+        neg = true;
+        start = 1;
+    }
+
+    for (size_t i = start; i < str.size(); ++i) {
+        *this *= Int128(10);
+        *this += Int128(str[i] - '0');
+    }
+
+    if (neg) {
+        *this = -*this;
+    }
 }
 
 Int128::operator int64_t() const {
@@ -14,11 +33,13 @@ Int128::operator int64_t() const {
 }
 
 Int128::operator double() const {
-    return std::ldexp(static_cast<double>(high_), 64) + static_cast<double>(low_);
+    double high = std::ldexp(static_cast<double>(high_), 64);
+    double low = static_cast<double>(static_cast<uint64_t>(low_));
+    return high + low;
 }
 
 std::string Int128::str() const {
-    if (*this == Int128(0)) { return "0"; }
+    if (high_ == 0 && low_ == 0) { return "0"; }
     Int128 temp = *this;
     bool neg = temp.high_ < 0;
     if (neg) { temp = -temp; } // not to have problems with /
@@ -27,7 +48,7 @@ std::string Int128::str() const {
     while (temp != Int128(0)) {
         Int128 q = temp / Int128(10);
         Int128 r = temp - q * Int128(10); // for not to write operator %
-        int64_t digit = static_cast<int64_t>(r);
+        int digit = static_cast<int>(static_cast<uint64_t>(r.low_));
         s.push_back('0' + digit);
         temp = q;
     }
@@ -38,113 +59,183 @@ std::string Int128::str() const {
 }
 
 Int128& Int128::operator+=(const Int128& other) {
-    uint64_t old = low_;
+    uint64_t old_low = low_;
     low_ += other.low_;
-    high_ += other.high_;
 
-    if (low_ < old) { high_ += 1; }
+    uint64_t carry = (low_ < old_low);
+
+    uint64_t new_high = static_cast<uint64_t>(high_) + static_cast<uint64_t>(other.high_) + carry;
+    high_ = static_cast<int64_t>(new_high);
+
     return *this;
 }
 
 Int128& Int128::operator-=(const Int128& other) {
-    uint64_t old = low_;
-    low_ -= other.low_;
-    high_ -= other.high_;
-
-    if (low_ > old) { high_ -= 1; }
+    *this += (-other);
     return *this;
-}
-
-Int128 Int128::operator-() const {
-    return Int128(0) - *this;
 }
 
 Int128& Int128::operator*=(const Int128& other) {
-    uint64_t al = static_cast<uint32_t>(low_); 
-    uint64_t ah = low_ >> 32;
-    uint64_t bl = static_cast<uint32_t>(other.low_); 
-    uint64_t bh = other.low_ >> 32;
+    Int128 result(0);
+    Int128 l = *this;
+    Int128 r = other;
 
-    uint64_t p0 = al * bl;
-    uint64_t p1 = al * bh;
-    uint64_t p2 = ah * bl;
-    uint64_t p3 = ah * bh;
+    bool neg = false;
 
-    uint64_t middle = (p0 >> 32) + static_cast<uint32_t>(p1) + p2;
-    
-    uint64_t new_low = (middle << 32) | static_cast<uint32_t>(p0);
-    uint64_t new_high = p3 + (middle >> 32) + (p1 >> 32);
-
-    new_high += (static_cast<uint64_t>(high_) * other.low_) + (low_ * static_cast<uint64_t>(other.high_));
-
-    low_ = new_low;
-    high_ = static_cast<int64_t>(new_high);
-    
-    return *this;
-}
-
-
-Int128& Int128::operator/=(const Int128& other) {
-    bool neg = (high_ < 0) ^ (other.high_ < 0);
-
-    Int128 dividend = *this;
-    Int128 divisor = other;
-
-    if (dividend.high_ < 0) dividend = -dividend;
-    if (divisor.high_ < 0) divisor = -divisor;
-
-    Int128 quotient(0);
-    Int128 remainder(0);
-
-    for (int i = 127; i >= 0; --i) {
-
-        remainder = remainder * Int128(2);
-
-        if (i >= 64) {
-            remainder.low_ |= (dividend.high_ >> (i - 64)) & 1;
-        } else {
-            remainder.low_ |= (dividend.low_ >> i) & 1;
-        }
-
-        if (!(remainder < divisor)) {
-            remainder -= divisor;
-
-            if (i >= 64) {
-                quotient.high_ |= (1ULL << (i - 64));
-            } else {
-                quotient.low_ |= (1ULL << i);
-            }
-        }
+    if (l.high_ < 0) {
+        l = -l;
+        neg = !neg;
+    }
+    if (r.high_ < 0) {
+        r = -r;
+        neg = !neg;
     }
 
-    if (neg) quotient = -quotient;
+    while (r != Int128(0)) {
+        if ((r.low_ & 1) != 0) {
+            result += l;
+        }
+        l += l;
+        r >>= 1;
+    }
 
-    *this = quotient;
+    if (neg) { result = -result; }
+
+    *this = result;
     return *this;
 }
 
-bool operator==(const Int128& a,const Int128& b) { return a.high_ == b.high_ && a.low_ == b.low_; }
+Int128& Int128::operator/=(const Int128& other) {
+    bool neg = false;
 
-bool operator!=(const Int128& a,const Int128& b){ return !(a == b); } 
+    Int128 result(0);
+    Int128 l = *this;
+    Int128 r = other;
 
-std::ostream& operator<<(std::ostream& os,const Int128& v) {
-    os << v.str();
-    return os;
+    if (l.high_ < 0) {
+        l = -l;
+        neg = !neg;
+    }
+    if (r.high_ < 0) {
+        r = -r;
+        neg = !neg;
+    }
+
+    while (r <= l) {
+        Int128 temp = r;
+        Int128 temp2 = Int128(1);
+
+        while ((temp << 1) <= l) {
+            temp <<= 1;
+            temp2 <<= 1;
+        }
+
+        l -= temp;
+        result += temp2;
+    }
+
+    if (neg) { result = -result; }
+
+    *this = result;
+    return *this;
 }
 
-Int128 operator+(Int128 a,const Int128& b){return a+=b;}
-
-Int128 operator-(Int128 a,const Int128& b){return a-=b;}
-
-Int128 operator*(Int128 a,const Int128& b){return a*=b;}
-
-Int128 operator/(Int128 a,const Int128& b){return a/=b;}
-
-bool operator<(const Int128& a, const Int128& b) {
-    if (a.high_ != b.high_) return a.high_ < b.high_;
-    return a.low_ < b.low_;
+Int128 Int128::operator+(const Int128& other) const {
+    Int128 temp = *this;
+    temp += other;
+    return temp;
 }
 
-bool operator>=(const Int128& a, const Int128& b) {
-    return !(a < b);
+Int128 Int128::operator-(const Int128& other) const {
+    Int128 temp = *this;
+    temp -= other;
+    return temp;
+}
+Int128 Int128::operator*(const Int128& other) const {
+    Int128 temp = *this;
+    temp *= other;
+    return temp;
+}
+Int128 Int128::operator/(const Int128& other) const {
+    Int128 temp = *this;
+    temp /= other;
+    return temp;
+}
+
+Int128 Int128::operator-() const {
+    Int128 result;
+    result.low_ = ~low_;
+    result.high_ = ~high_;
+    result += Int128(1);
+    return result;
+}
+
+bool Int128::operator==(const Int128& other) { return high_ == other.high_ && low_ == other.low_; }
+bool Int128::operator!=(const Int128& other) { return !(*this == other); } 
+
+std::ostream& operator<<(std::ostream& os, const Int128& v) { return os << v.str(); }
+
+bool Int128::operator<(const Int128& other) {
+    if (high_ != other.high_) { return high_ < other.high_; }
+    return low_ < other.low_;
+}
+
+bool Int128::operator<=(const Int128& other) {
+    return *this < other || *this == other;
+}
+
+bool Int128::operator>(const Int128 &other) {
+    if (high_ != other.high_) { return other.high_ < high_; }
+    return other.low_ < low_;
+}
+bool Int128::operator>=(const Int128 &other) {
+    return *this > other || *this == other;
+}
+
+Int128& Int128::operator<<=(int shift) {
+    if (shift >= 128) {
+        high_ = 0;
+        low_ = 0;
+        return *this;
+    }
+
+    if (shift >= 64) {
+        high_ = static_cast<int64_t>(low_ << (shift - 64));
+        low_ = 0;
+    } else if (shift > 0) {
+        high_ = (high_ << shift) | (low_ >> (64 - shift));
+        low_ <<= shift;
+    }
+    return *this;
+}
+
+Int128 Int128::operator<<(int shift) const {
+    Int128 temp = *this;
+    temp <<= shift;
+    return temp;
+}
+
+Int128& Int128::operator>>=(int shift) {
+    if (shift >= 128) {
+        if (high_ < 0) {  high_ = -1; }
+        else { high_ = 0; }
+        low_ = 0;
+        return *this;
+    }
+
+    if (shift >= 64) {
+        low_ = static_cast<uint64_t>(high_ >> (shift - 64));
+        if (high_ < 0) {  high_ = -1; }
+        else { high_ = 0; }
+    } else if (shift > 0) {
+        low_ = (low_ >> shift) | (static_cast<uint64_t>(high_) << (64 - shift));
+        high_ >>= shift;
+    }
+    return *this;
+}
+
+Int128 Int128::operator>>(int shift) const {
+    Int128 temp = *this;
+    temp >>= shift;
+    return temp;
 }
